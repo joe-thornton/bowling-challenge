c05f6ff3e5cb5debae379a4fe7471ff0f9b30ff1
Joe Thornton <thornton.jp@gmail.com>
2022-04-01 19:28:01 +0100

    Tests passing for 2 rolls of 1

--- game.js
+++ game.js
@@ -1,9 +1,20 @@
 class Game {
   constructor() {
-    this.scorecard = [null];
+    this.scorecard = [];
+    this.rolls = [];
   }
 
-  roll(pins) {}
+  roll(pins) {
+    this.rolls.push(1);
+    if (this.rolls.length % 2 === 0) {
+      this.scorecard.push(
+        this.rolls.reduce(
+          (previousValue, currentValue) => previousValue + currentValue,
+          0
+        )
+      );
+    }
+  }
 }
 
 module.exports = Game;
--- game.test.js
+++ game.test.js
@@ -1,9 +1,15 @@
-const Game = require("./game.js");
+const Game = require("./game");
 
 describe("game", () => {
   it("rolls a single figure roll and returns no score", () => {
     game = new Game();
     game.roll(1);
-    expect(game.scorecard).toEqual([null]);
+    expect(game.scorecard).toEqual([]);
+  });
+  it("rolls a single figure roll and returns no score", () => {
+    game = new Game();
+    game.roll(1);
+    game.roll(1);
+    expect(game.scorecard).toEqual([2]);
   });
 });
